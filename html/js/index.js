abe1692724a136f7eec5b19b7679a58fe3095683
Abdul2944 <abdul.bscs2944@gmail.com>
2023-07-24 22:46:38 +0100

    js file created

---
+++ html/js/index.js
@@ -0,0 +1,12 @@
+const searchBtn=document.querySelector('.searchBtn');
+const searchValue=document.querySelector('.search');
+
+
+var value=null;
+searchValue.addEventListener('input',(data)=>{value=null; value=data.target.value;});
+
+
+searchBtn.addEventListener('click',()=>{
+    window.reload;
+    window.location.href=`/api/users/search/${value}`;
+});
