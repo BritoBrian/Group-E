5fc68d405c63eb672413ac119bdb9697196f1c0b
BritoBrian <briankog23@gmail.com>
2023-07-25 19:01:55 +0100

    body-parser-initiliaze

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const dotenv=require('dotenv');
 const bodyparser=require('body-parser');
 const morgan=require('morgan');
 const path=require('path');
+
+const host='0.0.0.0';
+app.use(bodyparser.urlencoded({extended:true}));
