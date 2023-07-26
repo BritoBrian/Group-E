d9040425ede8d1da53de2c57ef35b7585a72139f
Anand4554 <anandjayara@gmail.com>
2023-07-26 10:58:21 +0100

    filerefactor

--- server/databases/connection.js
+++ server/databases/connection.js
@@ -3,4 +3,7 @@ const dotenv=require('dotenv');
 dotenv.config({path:'config.env'});
 
 const connectDb=mongoose.connect(process.env.MONGO_URI,{
-    dbName:'UserManagementSystem',}).then(console.log('database connected')).catch(e=>console.log(e));
+    dbName:'UserManagementSystem',
+}).then(console.log('database connected')).catch(e=>console.log(e));
+
+module.exports=connectDb;
