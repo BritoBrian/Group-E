7ac586ea53330edc6d4e78b0db4be144393237aa
Anand4554 <anandjayara@gmail.com>
2023-07-26 10:59:27 +0100

    homeroutesdefined

--- server/services/render.js
+++ server/services/render.js
@@ -1,2 +1,11 @@
 const axios=require('axios');
 const userDb=require('../model/model.js');
+
+// create a service to render homepage
+const homeroutes=(req,res)=>{
+    const db=userDb.find({}).then(data=>
+    axios.get('http://0.0.0.0:8000/api/users').then((resp)=>{
+        console.log(resp.data);
+        res.render('index',{users:resp.data})}).catch(e=>res.send(e)));
+
+}
