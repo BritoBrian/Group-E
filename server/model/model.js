962d73857f9b36bcb8c165e77cf8c560af9bb25f
Anand4554 <anandjayara@gmail.com>
2023-07-26 11:04:32 +0100

    schemedefined

--- server/model/model.js
+++ server/model/model.js
@@ -1,1 +1,14 @@
 const mongoose=require('mongoose');
+const scheme=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    gender:String,
+    status:String,
+    });
