e0ca6e848f59d197192f45832fa2511a2731cae4
BritoBrian <131865661+BritoBrian@users.noreply.github.com>
2023-07-26 17:28:36 +0100

    Update render.js

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,8 @@ const userDb=require('../model/model.js');
 // create a service to render homepage
 const homeroutes=(req,res)=>{
     const db=userDb.find({}).then(data=>
-    axios.get('http://0.0.0.0:80/api/users').then((resp)=>{
+    axios.get('https://user-management-system-igml.onrender.com
+/api/users').then((resp)=>{
         console.log(resp.data);
         res.render('index',{users:resp.data})}).catch(e=>res.send(e)));
 
@@ -16,7 +17,8 @@ const add_user=(req,res)=>{
 const update_user=(req,res)=>{
     // res.render('update_user');
     console.log('update');
-    axios.get('http://0.0.0.0:80/api/users',{params:{id:req.query.id}})
+    axios.get('https://user-management-system-igml.onrender.com
+/api/users',{params:{id:req.query.id}})
     .then((data)=>{
      res.render('update_user',{user:data.data})}).catch(e=>res.send(e));
 }
