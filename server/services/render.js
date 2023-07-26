1d5f7f922e52bc10a834d0443634b319206037aa
Anand4554 <anandjayara@gmail.com>
2023-07-26 11:00:14 +0100

    update_user

--- server/services/render.js
+++ server/services/render.js
@@ -13,3 +13,11 @@ const homeroutes=(req,res)=>{
 const add_user=(req,res)=>{
     res.render('new_User');
 }
+
+// create a service to render update_User page
+const update_user=(req,res)=>{
+    // res.render('update_user');
+    axios.get('http://0.0.0.0:8000/api/users',{params:{id:req.query.id}})
+    .then((data)=>{
+     res.render('update_user',{user:data.data})}).catch(e=>res.send(e));
+}
