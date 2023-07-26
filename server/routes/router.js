49acf0c1d2bb1457e8266c716086f98ae3f458fb
BritoBrian <briankog23@gmail.com>
2023-07-26 10:50:52 +0100

    router2

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,2 +1,8 @@
 const express=require('express');
 const route=express.Router();
+
+// Route to connect to homepage
+route.get('/',services.homeroutes);
+
+// Route to connect to add_user page
+route.get('/add-user',services.add_user);
