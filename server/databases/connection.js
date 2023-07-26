400795274e2c8dfd1e121821a3613310cfbe35b6
Anand4554 <anandjayara@gmail.com>
2023-07-26 10:57:44 +0100

    dotenv

--- server/databases/connection.js
+++ server/databases/connection.js
@@ -1,2 +1,6 @@
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
+dotenv.config({path:'config.env'});
+
+const connectDb=mongoose.connect(process.env.MONGO_URI,{
+    dbName:'UserManagementSystem',
